b0afb53fdb6eeaa7ecab2733891517fee151d319
gdphmcenter <112165672+gdphmcenter@users.noreply.github.com>
2024-03-17 13:40:33 +0800

    Add files via upload

---
+++ DAN.py
@@ -0,0 +1,41 @@
+
+import torch
+
+def guassian_kernel(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+    n_samples = int(source.size()[0])+int(target.size()[0])
+    total = torch.cat([source, target], dim=0)
+    total0 = total.unsqueeze(0).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+    total1 = total.unsqueeze(1).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+    L2_distance = ((total0-total1)**2).sum(2)
+    if fix_sigma:
+        bandwidth = fix_sigma
+    else:
+        bandwidth = torch.sum(L2_distance.data) / (n_samples**2-n_samples)
+    bandwidth /= kernel_mul ** (kernel_num // 2)
+    bandwidth_list = [bandwidth * (kernel_mul**i) for i in range(kernel_num)]
+    # print("bandwidth_list",bandwidth_list)
+    kernel_val = [torch.exp(-L2_distance / bandwidth_temp) for bandwidth_temp in bandwidth_list]
+    return sum(kernel_val)#/len(kernel_val)
+
+
+def DAN(source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+    batch_size = int(source.size()[0])
+    kernels = guassian_kernel(source, target,
+        kernel_mul=kernel_mul, kernel_num=kernel_num, fix_sigma=fix_sigma)
+    # print('kernel:',kernels)
+    XX = kernels[:batch_size, :batch_size]
+    YY = kernels[batch_size:, batch_size:]
+    XY = kernels[:batch_size, batch_size:]
+    YX = kernels[batch_size:, :batch_size]
+    # print('xx.shape:',XX)
+    loss = torch.mean(XX + YY - XY - YX)
+    # print('loss:',loss)
+    loss_1 = (XY[0,1] + XY[1,0] + YX[0,1] + YX[1,0])/4
+    # print('loss_1:',loss_1)
+    return loss
+
+if __name__ == '__main__':
+    source = torch.arange(6).reshape(2, 3).float()
+    target = torch.arange(6, 12).reshape(2, 3).float()
+    DAN(source,target)
+
